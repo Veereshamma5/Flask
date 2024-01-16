0d71e191d24d63c42fad93b28dab54714c6f08ff
Veereshamma5 <141397180+Veereshamma5@users.noreply.github.com>
2024-01-16 22:35:53 +0530

    Update run.py

--- run.py
+++ run.py
@@ -29,31 +29,5 @@ def cdp_app_before_request():
     req_logger.info(json.dumps(log_params))
 
 
-"""@cdp_app.route("/swag", methods=['GET'])
-def spec():
-    swag = swagger(cdp_app)
-    swag['info']['version'] = '1.0'
-    swag['info']['title'] = 'Your API'
-    return jsonify(swag)"""
-
-
-@cdp_app.route("/square/<int:num>", methods=['GET'])
-def square(num):
-    """
-        This endpoint returns the square of the provided number.
-        ---
-        parameters:
-          - name: num
-            in: path
-            type: integer
-            required: true
-            description: The number to square.
-        responses:
-          200:
-            description: The square of the number.
-        """
-    return jsonify({'result': num * num}), 200
-
-
 if __name__ == "__main__":
     cdp_app.run(debug=True)
